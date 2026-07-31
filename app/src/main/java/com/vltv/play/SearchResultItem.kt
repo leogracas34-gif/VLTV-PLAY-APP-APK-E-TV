7764c56dcd9eadf8ec43d2a99a40a5bb21b95a00
leogracas34-gif <leogracas34@gmail.com>
2026-07-31 15:51:50 -0300

    Delete app/src/main/java directory

--- app/src/main/java/com/vltv/play/SearchResultItem.kt
+++
@@ -1,9 +0,0 @@
-package com.vltv.play
-
-data class SearchResultItem(
-    val id: Int,
-    val title: String,
-    val type: String,      // "live", "movie", "series"
-    val extraInfo: String? = null,
-    val iconUrl: String? = null
-)
