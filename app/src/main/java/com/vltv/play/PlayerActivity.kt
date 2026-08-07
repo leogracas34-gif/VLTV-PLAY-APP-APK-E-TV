b9861403d3537c7108908871cbf9dd1a0ef2c5c4
leogracas34-gif <leogracas34@gmail.com>
2026-08-07 07:50:27 -0300

    Add import for withContext from kotlinx.coroutines

--- app/src/main/java/com/vltv/play/PlayerActivity.kt
+++ app/src/main/java/com/vltv/play/PlayerActivity.kt
@@ -47,6 +47,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import com.vltv.play.data.AppDatabase
 import com.vltv.play.data.WatchHistoryEntity
 
